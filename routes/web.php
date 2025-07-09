5ab1a2bf1fac482a6f895fbf8a879cae497ef6fd
Matias Vera <Matias18698@gmail.com>
2025-07-09 02:06:12 -0400

    cambia ruta inicial

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,14 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
+    /*
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
-    ]);
+    ]);*/
+    redirect()->route('dashboard');
 });
 //ruta para el cron de asistencias
 Route::get('/Asistencias/cron', [AsistenciaController::class, 'AsistenciaCron'])->name('asistencia.cron'); // NUEVA
