e9907960e399ef068864cbc19ce3b51585cafee6
Matias Vera <Matias18698@gmail.com>
2025-07-18 18:30:04 -0400

    queita base en vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     
-    //esto cambiarlo cada vez que haga un commit base:'/tesisss/public/build', 
+    //esto cambiarlo cada vez que haga un commit 
+    //base:'/tesisss/public/build', 
     plugins: [
         laravel({
             input: '/resources/js/app.js',
